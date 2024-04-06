5ca168644b2da2e5fcf08e96464db360a437231e
Abhinav Adarsh <114936367+imabhinavadarsh@users.noreply.github.com>
2024-04-06 12:22:20 +0530

    regex for Bucket Question
    Steps Performed
    //Declared Scanner
    //public mehtod which I am sharing to make the code concise
    //seperate method of integer Bucket
    //seperate method of alphabet Bucket
    //seperate method of symbol Bucket
    //created a method which contains all the methods accoring to our need we can invoke the particular method 
    //create a object of class BucketyQuestion and invoking mall method

---
+++ BucketQuestion/byRegex.java
@@ -0,0 +1,48 @@
+import java.util.*;
+class BucketQuestion{
+  //Declared Scanner
+    Scanner sc = new Scanner(System.in);
+  
+//public mehtod which I am sharing to make the code concise
+    public void ScannerToShare(){
+        System.out.println("Enter the mix string");
+    }
+        //seperate method of integer Bucket
+    void integerBucket(){
+        ScannerToShare();
+        String s = sc.nextLine();
+        String intBucket = s.replaceAll("[^0-9]","");
+        System.out.println("Number Bucket :" + intBucket);
+        }
+  
+        //seperate method of alphabet Bucket
+        void alphabetBucket(){
+        ScannerToShare();
+        String s = sc.nextLine();
+        String alphaBucket = s.replaceAll("[^a-zA-Z]","");
+        System.out.println("Alphabet Bucket" + alphaBucket);
+        }
+  
+        //seperate method of symbol Bucket
+        void specialCharacterBucket(){
+        ScannerToShare();
+        String s = sc.nextLine();
+        String specialCharBucket = s.replaceAll("[0-9a-zA-Z]","");
+        System.out.println("SpecialChar Bucket :" + specialCharBucket);
+        }
+
+        //created a method which contains all the methods accoring to our need we can invoke the particular method 
+        void mall(){
+            integerBucket();
+            alphabetBucket();
+            specialCharacterBucket();
+        }
+
+    }
+public class Main{
+    public static void main(String [] args){
+      //create a object of class BucketyQuestion and invoking mall method 
+     BucketQuestion bq = new BucketQuestion();
+     bq.mall();
+    }
+}
