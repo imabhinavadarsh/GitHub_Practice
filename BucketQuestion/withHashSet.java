582d03490ed2d305119742b29c6ee29aacc00056
Abhinav Adarsh <114936367+imabhinavadarsh@users.noreply.github.com>
2024-04-07 21:20:53 +0530

    Create with HashSet
    HashSet used -> It stored the value in the form of hashcode. So no duplicates allowed.

---
+++ BucketQuestion/withHashSet.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class DuplicateRemoveHS{
+    void duplicateHS(){
+        try(Scanner sc = new Scanner(System.in)){
+            System.out.println("Enter a String");
+            String s = sc.nextLine();
+            
+            if(s.isEmpty()){
+                System.out.println("Empty String");
+                return;
+            }
+            
+            HashSet<Character> hs = new HashSet<>();
+            for(char c : s.toCharArray()){
+                hs.add(c);
+            }
+            System.out.println("Duplicate Removed :" + hs);
+        }
+        catch(InputMismatchException e){
+            System.out.println("ERROR IN INPUT!" + e.getMessage());
+        }
+    }
+}
+public class Main{
+    public static void main(String[] args){
+        DuplicateRemoveHS dr = new DuplicateRemoveHS();
+        dr.duplicateHS();
+    }
+}
