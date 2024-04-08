0b73c6c92d8a0db56235619838ca00962f1091af
Abhinav Adarsh <114936367+imabhinavadarsh@users.noreply.github.com>
2024-04-09 00:04:39 +0530

    get Element By Index

---
+++ List/getElementByIndex.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main{
+    public static void main(String[]args){
+        ArrayList<Integer> al = new ArrayList<>();
+        //Adding to an ArrayList
+        al.add(10);
+        al.add(20);
+        al.add(9);
+        al.add(5);
+        System.out.println(al);
+      //get element by index
+        System.out.println(al.get(1));
+
+    }
+}
