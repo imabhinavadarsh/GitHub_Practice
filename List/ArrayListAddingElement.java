ba66fa98df7dce4db9e542cd861d868744988e3d
Abhinav Adarsh <114936367+imabhinavadarsh@users.noreply.github.com>
2024-04-08 23:58:30 +0530

    Array List Adding Element.

---
+++ List/ArrayListAddingElement.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main{
+    public static void main(String[]args){
+        ArrayList<Integer> al = new ArrayList<>();
+        //Adding to an ArrayList
+        al.add(10);
+        al.add(20);
+        al.add(9);
+        al.add(5);
+        System.out.println(al);
+    }
+}
