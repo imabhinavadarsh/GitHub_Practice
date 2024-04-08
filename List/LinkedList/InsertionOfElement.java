869798340a613ca0f1c2bb4632a05b7cd7516d1d
Abhinav Adarsh <114936367+imabhinavadarsh@users.noreply.github.com>
2024-04-09 01:19:23 +0530

    Insertion Of Element

--- List/LinkedList/InsertionOfElement.java
+++ List/LinkedList/InsertionOfElement.java
@@ -1,1 +1,17 @@
+import java.util.*;
+public class Main{
+    public static void main(String[]args){
+        LinkedList<Integer> ll = new LinkedList<>();
+        //Adding to an ArrayList
+        ll.add(10);
+        ll.add(20);
+        ll.add(9);
+        ll.add(5);
+        System.out.println(ll);
+        //Performing insertion 
+        ll.set(2, 89);
+        //Here 2 is the index and element I am inserting
+        System.out.println(ll);
 
+    }
+}
